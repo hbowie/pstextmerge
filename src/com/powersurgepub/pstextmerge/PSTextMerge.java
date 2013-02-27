271e912476cdc21e204ca3bbdb218bcab4a26e59
Herb Bowie <hbowie@mac.com>
2013-02-27 04:55:01 -0800

    Updated version number to 4.10
    Prepping for release
    
    Signed-off-by: Herb Bowie <hbowie@mac.com>

--- src/com/powersurgepub/pstextmerge/PSTextMerge.java
+++ src/com/powersurgepub/pstextmerge/PSTextMerge.java
@@ -72,7 +72,7 @@ public class PSTextMerge
   
   /** Program Name */
   public    static  final String  PROGRAM_NAME = "PSTextMerge";
-  public    static  final String  PROGRAM_VERSION = "4.10 - 2013-02-11";
+  public    static  final String  PROGRAM_VERSION = "4.10";
   
   private   static  final String  USER_GUIDE
       = "userguide/pstextmerge.html";
