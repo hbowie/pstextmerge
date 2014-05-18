23d9f85d7f168b3a23aa622a153c7cd2af902554
Herb Bowie <hbowie@mac.com>
2014-05-18 15:46:03 -0700

    Changes to Change Log format and Updated to version 4.30
    Signed-off-by: Herb Bowie <hbowie@mac.com>

--- src/com/powersurgepub/pstextmerge/PSTextMerge.java
+++ src/com/powersurgepub/pstextmerge/PSTextMerge.java
@@ -71,7 +71,7 @@ public class PSTextMerge
   
   /** Program Name */
   public    static  final String  PROGRAM_NAME = "PSTextMerge";
-  public    static  final String  PROGRAM_VERSION = "4.20";
+  public    static  final String  PROGRAM_VERSION = "4.30";
   
   private   static  final String  USER_GUIDE
       = "userguide/pstextmerge.html";
